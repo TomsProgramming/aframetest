1328b40378d4347279070d13315ef28522faf55a
Tom Tiedemann <tomtiedemann30@gmail.com>
2025-01-27 09:00:14 +0100

    update

--- script.js
+++ script.js
@@ -1,38 +1,77 @@
 let currentDoor = null;
+let openingDoor = false;
+
 
 document.addEventListener("DOMContentLoaded", function() {
-    currentDoor = document.querySelector("#door3");
     let door3 = document.querySelector("#door3");
 
     door3.addEventListener("click", function() {
-        console.log("De deur is geselecteerd!");
         selectDoor(this); 
     });
 
-    document.querySelector("#slider1").addEventListener("input", function() {
-        let door3Scale = document.querySelector("#door3").getAttribute("scale");
-        door3Scale.z = this.value;
-        document.querySelector("#door3").setAttribute("scale", door3Scale);
+    document.querySelector("#sliderHeight").addEventListener("input", function() {
+        if(currentDoor != null){
+            let currentScale = currentDoor.getAttribute("scale");
+            let scale = this.value;
+            currentScale.y = scale;
+            currentDoor.setAttribute("scale", `${currentScale.x} ${currentScale.y} ${currentScale.z}`);
+            document.querySelector(`#${currentDoor.id}-selected`).setAttribute("scale", `${currentScale.x} ${currentScale.y + 0.05} ${currentScale.z + 0.05}`);
+        }
+    });
+
+    document.querySelector("#sliderWidth").addEventListener("input", function() {
+        if(currentDoor != null){
+            let currentScale = currentDoor.getAttribute("scale");
+            let scale = this.value;
+            currentScale.z = scale;
+            currentDoor.setAttribute("scale", `${currentScale.x} ${currentScale.y} ${currentScale.z}`);
+            document.querySelector(`#${currentDoor.id}-selected`).setAttribute("scale", `${currentScale.x + 0.05} ${currentScale.y} ${currentScale.z + 0.05}`);
+        }
+    });
+
+    document.querySelector('#openDoor').addEventListener("click", function() {
+        if(currentDoor != null){
+            openDoor();
+        }
     });
 });
 
+function deselectDoor(){
+    if(currentDoor != null){
+        document.querySelector(`#${currentDoor.id}-selected`).setAttribute("opacity", "0");
+    }
+}
+
 function selectDoor(element){
-    console.log("De deur is geselecteerd!");
-    currentDoor = element;
+    if(currentDoor != element){
+      deselectDoor();
+      document.querySelector("#selectedDoor").innerHTML = element.id;
+      document.querySelector(`#${element.id}-selected`).setAttribute("opacity", "0.1");
+      currentDoor = element;
+    }else{
+      deselectDoor();
+      currentDoor = null;
+      document.querySelector("#selectedDoor").innerHTML = "None";
+    }
 }
 
 function openDoor() {
-    let currentRotation = currentDoor.getAttribute("rotation"); // Get current rotation of the currentDoor
-    let targetRotation = currentRotation.y - 90; // Rotate 90 degrees (adjust as needed)
-    let step = -1; // Rotation step
+    let currentRotation = currentDoor.getAttribute("rotation");
+    let targetRotation = currentRotation.y - 90; 
+    let step = -1; 
 
     function animateRotation() {
       if (currentRotation.y > targetRotation) {
-        currentRotation.y += step; // Decrease the rotation
+        openingDoor = true;
+        currentRotation.y += step; 
         currentDoor.setAttribute("rotation", `${currentRotation.x} ${currentRotation.y} ${currentRotation.z}`);
-        setTimeout(animateRotation, 10); // Smooth animation
+        setTimeout(animateRotation, 10); 
+      }else{
+        openingDoor = false;
       }
     }
 
-    animateRotation();
-  }
+    if(openingDoor == false){
+        animateRotation();
+    }
+}
